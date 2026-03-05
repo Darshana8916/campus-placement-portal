93c7f0a398b0e46ba893c3daf537b4d347c3c957
Vaishnavi-Pawar05 <pawarvaishnavi073@gmail.com>
2026-03-05 18:00:06 +0530

    for nextbutton js added

---
+++ js/student-registration.js
@@ -0,0 +1,8 @@
+let currentStep = 1;
+
+function nextStep(from) {
+    const valid = from === 1 ? validateStep1() : from === 2 ? validateStep2() : true;
+    if (!valid) return;
+    currentStep = from + 1;
+    updateStepUI(currentStep);
+  }
